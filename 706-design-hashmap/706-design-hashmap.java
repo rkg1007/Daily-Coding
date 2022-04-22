b348ac5b2617eccbf841a4792ba93fd11803ce7a
Rohit <99075474+rkg1007@users.noreply.github.com>
2022-04-22 08:52:54 +0530

    Time: 20 ms (80.12%), Space: 63.7 MB (42.62%) - LeetHub

---
+++ 706-design-hashmap/706-design-hashmap.java
@@ -0,0 +1,60 @@
+class MyHashMap {
+    Node root;
+    
+    private class Node {
+        Node[] arr;
+        int val;
+        
+        Node() {
+            this.arr = new Node[10];
+            this.val = -1;
+        }
+    }
+
+    public MyHashMap() {
+        this.root = new Node();
+    }
+    
+    public void put(int key, int value) {
+        Node curr = this.root;
+        while (key > 0) {
+            int idx = key % 10;
+            key /= 10;
+            if (curr.arr[idx] == null) curr.arr[idx] = new Node();
+            curr = curr.arr[idx];
+        }
+        curr.val = value;
+        return;
+    }
+    
+    public int get(int key) {
+        Node curr = this.root;
+        while (key > 0) {
+            int idx = key % 10;
+            key /= 10;
+            if (curr.arr[idx] == null) return -1;
+            curr = curr.arr[idx];
+        }
+        return curr.val;
+    }
+    
+    public void remove(int key) {
+        Node curr = this.root;
+        while (key > 0) {
+            int idx = key % 10;
+            key /= 10;
+            if (curr.arr[idx] == null) return;
+            curr = curr.arr[idx];
+        }
+        curr.val = -1;
+        return;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
