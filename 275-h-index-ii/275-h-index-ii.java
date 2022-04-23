45838319fba6ca81c0fbcddfed4dfffc7409a039
Rohit <99075474+rkg1007@users.noreply.github.com>
2022-04-23 12:32:28 +0530

    Time: 1 ms (22.17%), Space: 52.8 MB (16.25%) - LeetHub

---
+++ 275-h-index-ii/275-h-index-ii.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        for (int i = 0; i < n; i++) {
+            int h = n - i;
+            if (citations[i] >= h) return h;
+        }
+        return 0;
+    }
+}
