d6262210c1fc8037806b4609b2d00272c4e2dc7d
Rohit <99075474+rkg1007@users.noreply.github.com>
2022-04-23 09:53:48 +0530

    Time: 5 ms (19.85%), Space: 84.2 MB (84.07%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.java
@@ -1,9 +1,4 @@
 class Solution {
-    public void print(int[] arr) {
-        for(int num : arr) System.out.print(num + " ");
-        System.out.println();
-    }
-    
     public boolean increasingTriplet(int[] nums) {
         int n = nums.length;
         
@@ -12,14 +7,12 @@ class Solution {
             if (i == 0) left[i] = nums[i];
             else left[i] = Math.min(left[i-1], nums[i]);
         }
-        print(left);
         
         int[] right = new int[n];
         for (int i = n - 1; i >= 0; i--) {
             if (i == n - 1) right[i] = nums[i];
             else right[i] = Math.max(right[i+1], nums[i]);
         }
-        print(right);
         
         for (int i = 1; i < n - 1; i++) {
             if (left[i-1] < nums[i] && right[i+1] > nums[i]) {
