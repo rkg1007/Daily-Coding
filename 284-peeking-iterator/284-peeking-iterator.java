974edf93f8b6a412756cd957468d3d69600f5433
Rohit <99075474+rkg1007@users.noreply.github.com>
2022-04-25 08:39:18 +0530

    Time: 7 ms (39.41%), Space: 42.8 MB (58.43%) - LeetHub

--- 284-peeking-iterator/284-peeking-iterator.java
+++ 284-peeking-iterator/284-peeking-iterator.java
@@ -2,29 +2,29 @@
 // https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
 
 class PeekingIterator implements Iterator<Integer> {
-  Iterator<Integer> iterator;
-  int peek;
+    Iterator<Integer> iterator;
+    int peek;
   
 	public PeekingIterator(Iterator<Integer> iterator) {
-	  this.iterator = iterator;
-	  this.peek = this.iterator.hasNext() ? this.iterator.next() : -1;  
+	    this.iterator = iterator;
+	    this.peek = this.iterator.hasNext() ? this.iterator.next() : -1;  
 	}
 	
   
-  public Integer peek() {
-    return this.peek;     
-	}
+    public Integer peek() {
+        return this.peek;     
+    }
 	
 	
 	@Override
 	public Integer next() {
-	  int peekVal = this.peek;
-    this.peek = this.iterator.hasNext() ? this.iterator.next() : -1;
-    return peekVal;
+        int peekVal = this.peek;
+        this.peek = this.iterator.hasNext() ? this.iterator.next() : -1;
+        return peekVal;
 	}
 	
 	
 	public boolean hasNext() {
-	  return this.peek != -1;
+	    return this.peek != -1;
 	}
 }
