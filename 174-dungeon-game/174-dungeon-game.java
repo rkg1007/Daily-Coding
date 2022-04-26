5cfff9c040a019915d0a6b34c9d18e6bb7689407
Rohit <99075474+rkg1007@users.noreply.github.com>
2022-04-26 08:38:12 +0530

    Time: 92 ms (5.10%), Space: 47.4 MB (5.58%) - LeetHub

---
+++ 174-dungeon-game/174-dungeon-game.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean isValid(int[][] matrix, int x, int y) {
+        if (x >= 0 && x < matrix.length) {
+            if (y >= 0 && y < matrix[0].length) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public void print(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    } 
+    
+    public int calculateMinimumHP(int[][] dungeon) {
+        int m = dungeon.length, n = dungeon[0].length;
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int pathSum = 0, flag = 0;
+                if (isValid(dungeon, i + 1, j)) {
+                    pathSum = Math.min(0, dungeon[i + 1][j]);
+                    flag = 1;
+                }
+                if (isValid(dungeon, i, j + 1)) {
+                    if (flag == 1) pathSum = Math.max(pathSum, Math.min(0, dungeon[i][j + 1]));
+                    else pathSum = Math.min(0, dungeon[i][j + 1]);
+                }
+                dungeon[i][j] += pathSum;
+            }
+        }
+        
+        print(dungeon);
+        
+        if (dungeon[0][0] <= 0) {
+            return dungeon[0][0] * -1 + 1;
+        }
+        return 1;
+    }
+}
