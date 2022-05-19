fe7bbf13cab60b012f9f35e28f6fd1496d0e979f
Rohit <rkg10072000@gmail.com>
2022-05-19 09:49:02 +0530

    Time: 13 ms (47.41%), Space: 42.7 MB (82.03%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -1,37 +1,24 @@
 class Solution {
     
-    public boolean isValid(int[][] vis, int x, int y) {
-        if (x >= 0 && x < vis.length) {
-            if (y >= 0 && y < vis[0].length) {
-                return vis[x][y] == 0;
+    public boolean isValid(int[][] matrix, int x, int y) {
+        if (x >= 0 && x < matrix.length) {
+            if (y >= 0 && y < matrix[0].length) {
+                return true;
             }
         }
         return false;
     }
     
-    public void print(int[][] matrix) {
-        for (int[] row : matrix) {
-            for (int val : row) {
-                System.out.print(val + " ");
-            }
-            System.out.println();
-        }
-    }
-    
-    public int dfs(int[][] matrix, int x, int y, int[][] vis, int[][] dp, int lastVal) {
+    public int dfs(int[][] matrix, int x, int y, int[][] dp, int lastVal) {
         if (matrix[x][y] <= lastVal) return 0;
-        if (dp[x][y] == -1) {
-            dp[x][y] = 0;
-            vis[x][y] = 1;
+        if (dp[x][y] == 0) {
             int[][] steps = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
             for (int[] step : steps) {
                 int newX = x + step[0], newY = y + step[1];
-                if (isValid(vis, newX, newY)) {
-                    dp[x][y] = Math.max(dp[x][y], dfs(matrix, newX, newY, vis, dp, matrix[x][y]));
+                if (isValid(matrix, newX, newY)) {
+                    dp[x][y] = Math.max(dp[x][y], dfs(matrix, newX, newY, dp, matrix[x][y]) + 1);
                 }
             }
-            dp[x][y] += 1;
-            vis[x][y] = 0;
         }
         
         return dp[x][y];
@@ -39,15 +26,14 @@ class Solution {
     
     public int longestIncreasingPath(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
-        
-        int[][] vis = new int[m][n];
+       
         int[][] dp = new int[m][n];
-        for (int[] row : dp) Arrays.fill(row, -1);
+        for (int[] row : dp) Arrays.fill(row, 0);
         
         int ans = 1;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                ans = Math.max(ans, dfs(matrix, i, j, vis, dp, -1));
+                ans = Math.max(ans, dfs(matrix, i, j, dp, -1));
             }
         }
         
